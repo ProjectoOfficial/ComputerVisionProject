b51e6af0ce78dd6914ac7ae1a01cb773e2a20207
ProjectoOfficial <miniprojectsofficial@gmail.com>
2022-08-23 16:48:23 +0200

    Custom train and BDDDataset

--- Preprocessing.py
+++ Preprocessing.py
@@ -19,14 +19,21 @@ class Preprocessing():
 
     def __init__(self, size: tuple=(1280, 1280)):
         self.size = size
-        random.seed(7)
+
+        self.crop_transform = A.Compose([
+            A.Crop(0, 0, size[0], size[1]),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
         self.train_trainsform = A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.ShiftScaleRotate(p=0.5),
-        A.RandomBrightnessContrast(p=0.3),
-        A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=0.3),
-        ], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels']))
+            A.AdvancedBlur(p=0.2),
+            A.RandomFog(p=0.1),
+            A.RandomRain(p=0.1),
+            A.MotionBlur(p=0.5),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.3),
+            A.ColorJitter(p=0.2),
+            A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=0.3),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
     @staticmethod
     def GaussianBlur(frame: np.ndarray, sigma:float):
@@ -36,31 +43,41 @@ class Preprocessing():
     def to_np_frame(cls, frame: np.ndarray):
         return np.swapaxes(np.swapaxes(np.uint8(frame), 0, 2), 0, 1)
 
-    def Transform_base(self, frame: np.ndarray, boxes: np.ndarray, newsize: tuple, np_tensor: bool=False):
+    def Transform_base(self, frame: np.ndarray, labels: np.ndarray, np_tensor: bool=False):
         '''
         Transform_base contains image transformation used both on camera and dataset images. It takes images coming
         from different sources and and modifies them so that the output images all have the same structure
         ''' 
-        self.base_transform(frame, boxes)
+        
+        if frame.shape != (1280, 1280, 3):
+            if frame.size > np.ndarray((*self.size, 3), dtype=frame.dtype).size:
+                class_labels = labels[:, 0]
+                boxes = labels[:, 1:]
+                transformed = self.crop_transform(image=frame, bboxes=boxes, class_labels=class_labels)
 
-        if np_tensor:
-            frame = Preprocessing.to_np_frame(frame.cpu().detach().numpy())
+                frame = transformed['image']
+                transformed_bboxes = np.array(transformed['bboxes'])
+                transformed_class_labels = np.array(transformed['class_labels']).reshape(-1, 1)
 
-        return frame, boxes
+                if transformed_bboxes.size != 0 and transformed_class_labels.size != 0:
+                    labels = np.hstack((transformed_class_labels, transformed_bboxes))
+            else:
+                frame, labels = Preprocessing.pad_image(frame, labels)
+
+        return frame, labels
 
     def Transform_train(self, frame: np.ndarray, labels: np.ndarray) -> Union[np.ndarray, np.ndarray]:
         class_labels = labels[:, 0]
         boxes = labels[:, 1:]
 
-        frame = np.swapaxes(np.swapaxes(np.uint8(frame), 0, 2), 0, 1)
         transformed = self.train_trainsform(image=frame, bboxes=boxes, class_labels=class_labels)
 
         frame = transformed['image']
         transformed_bboxes = np.array(transformed['bboxes'])
         transformed_class_labels = np.array(transformed['class_labels']).reshape(-1, 1)
 
-        frame = np.swapaxes(np.swapaxes(np.uint8(frame), 0, 2), 2, 1)
-        labels = np.hstack((transformed_class_labels, transformed_bboxes))
+        if transformed_bboxes.size != 0 and transformed_class_labels.size != 0:
+            labels = np.hstack((transformed_class_labels, transformed_bboxes))
         return frame, labels
 
     @staticmethod
