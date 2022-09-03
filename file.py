e73ce9a8401d406fd2e3a8d2451ad4d5ee7203f3
ProjectoOfficial <miniprojectsofficial@gmail.com>
2022-09-03 15:28:46 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,17 @@
-import ftplib
+from ftplib import FTP, FTP_TLS
+import ipaddress
+import ssl
+from urllib.parse import uses_netloc
 import gdown
 import argparse, os
+import socket
+
+def check_ip(ip: str):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
 
 def dir_path(string):
     if os.path.isdir(string):
@@ -9,44 +20,42 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 #insert host. username and password
-session = ftplib.FTP('', '', '')
+def ftp_upload(host: str, username: str, password: str, path: str):
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    session = FTP()
+    session.set_pasv(True)
+    session.connect(host=host)
+    session.login(user=username, passwd=password)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--path', type=dir_path)
-args = parser.parse_args()
+    print(session.retrlines('LIST'))
 
-os.chdir(args.path)
-
-file = open('file.py','rb')                  # file to send
-session.storbinary('STOR file.py', file)     # send the file
-file.close()                                    # close file and FTP
-session.quit()
+    #file = open(path,'rb')                  # file to send
+    #session.storbinary('STOR ' + path, file)     # send the file
+    #file.close()                                    # close file and FTP
+    session.quit()
 
 #url = "https://drive.google.com/drive/folders/1rkoaZ-io_HjaeUgKoKMEQbq8vo0OQBgS"
 #gdown.download_folder(url, quiet=True, use_cookies=False)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--download', action="store_true", default=False)
+    parser.add_argument('-f', '--file', type=str)
+    parser.add_argument('-U', '--username', type=str)
+    parser.add_argument('-P', '--password', type=str)
+    parser.add_argument('-H', '--host', type=str)
+    args = parser.parse_args() 
 
+    print(args)
 
+    assert args.download is not None, 'specify if you want to download or upload a file'
+    assert args.username is not None and args.username != "", "Invalid username"
+    assert args.password is not None and args.password != "", "Invalid password"
+    if not args.download:
+        assert os.path.isfile(args.file), "invalid path to file (not a file?)"
+        assert args.host is not None and check_ip(args.host), "Invalid host address"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print('File caricato')
-
+    if not args.download:
+        ftp_upload(args.host, args.username, args.password, args.file)
+    else:
+        pass
