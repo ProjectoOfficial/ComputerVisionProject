ff5967f22bf8904335a273ac30ae3277567ab8d3
ferrarifilippo <filloferra7@gmail.com>
2022-09-03 17:48:33 +0200

    Modificato file.py

--- file.py
+++ file.py
@@ -3,6 +3,8 @@ import argparse, os
 import socket
 from ftplib import FTP 
 from tqdm import tqdm
+from google_drive_downloader import GoogleDriveDownloader
+from unrar import rarfile
 
 global FILE_SIZE
 global sizeWritten
@@ -15,12 +17,6 @@ def check_ip(ip: str):
         return True
     except socket.error:
         return False
-
-def dir_path(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise NotADirectoryError(string)
 
 #insert host. username and password
 def ftp_upload(host: str, username: str, password: str, path: str):
@@ -37,16 +33,25 @@ def ftp_upload(host: str, username: str, password: str, path: str):
 
     filesize = os.path.getsize(args.file)
     file = open(path, 'rb')
-    action = "STOR " + os.path.basename(path)
     with tqdm(unit = 'blocks', unit_scale = True, leave = False, miniters = 1, desc = 'Uploading......', total = filesize) as tqdm_instance:
         ftp.storbinary('STOR ' + os.path.basename(path), file, 2048, callback = lambda sent: tqdm_instance.update(len(sent)))
 
     file.close()
     ftp.close()
-   
 
-#url = "https://drive.google.com/drive/folders/1rkoaZ-io_HjaeUgKoKMEQbq8vo0OQBgS"
-#gdown.download_folder(url, quiet=True, use_cookies=False)
+def g_download(url: str, name: str, where: str):
+    file_id = url.rsplit('/', 1)[-1]
+
+    if name.endswith('rar'):
+        GoogleDriveDownloader.download_file_from_google_drive(file_id=file_id,
+                                                        dest_path=os.path.join(where, name))
+        rar = rarfile.RarFile(os.path.join(where, name))
+        rar.extractall()
+    
+    else:
+        GoogleDriveDownloader.download_file_from_google_drive(file_id=file_id,
+                                                        dest_path=os.path.join(where, name),
+                                                        unzip=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -55,20 +60,25 @@ if __name__ == '__main__':
     parser.add_argument('-U', '--username', type=str)
     parser.add_argument('-P', '--password', type=str)
     parser.add_argument('-H', '--host', type=str)
+    parser.add_argument('-g', '--g_url', type=str)
+    parser.add_argument('-n', '--name', type=str)
+    parser.add_argument('-w', '--where', type=str)
     args = parser.parse_args()
 
     print(args)
 
     assert args.download is not None, 'specify if you want to download or upload a file'
-    assert args.username is not None and args.username != "", "Invalid username"
-    assert args.password is not None and args.password != "", "Invalid password"
+
     if not args.download:
+        assert args.username is not None and args.username != "", "Invalid username"
+        assert args.password is not None and args.password != "", "Invalid password"
         assert os.path.isfile(args.file), "invalid path to file (not a file?)"
         assert args.host is not None and check_ip(args.host), "Invalid host address"
-
-    if not args.download:
         ftp_upload(args.host, args.username, args.password, args.file)
     else:
-        pass
+        assert args.g_url is not None and args.g_url != "", "Invalid google drive URL"
+        assert args.name is not None and args.name != "", "Invalid file name"
+        assert args.where is not None and os.path.isdir(args.where), "Invalid file path"
+        g_download(args.g_url, args.name, args.where)
 
-    
+    print("program terminated successfully!")
