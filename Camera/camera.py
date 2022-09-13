380b7be165102348b1f27ace2183ed09d5706bbb
ProjectoOfficial <miniprojectsofficial@gmail.com>
2022-09-13 17:34:01 +0200

    Revert "Merge branch 'main' of https://github.com/ProjectoOfficial/ComputerVisionProject"
    This reverts commit fb49c198804f3f7059880023b9fb98ac2f10a40d, reversing
    changes made to e89f7d0f1cdc606618cadba7e93d622d59430251.

--- Camera/camera.py
+++ Camera/camera.py
@@ -16,7 +16,7 @@ import csv
 
 from pynput.keyboard import Listener
 
-from Camera.RTCamera import RTCamera
+from RTCamera import RTCamera
 from Geometry import Geometry
 from Preprocessing import Preprocessing
 from Distance import Distance
@@ -26,6 +26,13 @@ from Tracking import Tracking
 
 from Models.YOLOv7.yolo_test import Test
 from Models.YOLOv7.utils.general import increment_path, non_max_suppression, scale_coords, xyxy2xywh
+
+PRESSED_KEY = ''
+RECORDING = False
+BLUR = False
+TRANSFORMS = False
+CHESSBOARD = False
+ROTATION = None
 
 def on_press(key):
     global PRESSED_KEY
@@ -37,14 +44,27 @@ def on_press(key):
 listener = Listener(on_press=on_press)
 preprocessor = Preprocessing((640, 640))
 
-def main(opt):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-    PRESSED_KEY = ''
-    RECORDING = False
-    BLUR = False
-    TRANSFORMS = False
-    CHESSBOARD = False
-    ROTATION = None
+    parser.add_argument('-b', '--batch-size', type=int, default=1, help='YOLOv7 batch-size')
+    parser.add_argument('-c', '--calibrate', action='store_true', default=False, help='true if you want to calibrate the camera')
+    parser.add_argument('-cd', '--camera-device', type=int, default=0, help='Camera device ID')
+    parser.add_argument('-ct', '--conf-thres', type=float, default=0.001, help='YOLOv7 conf threshold')
+    parser.add_argument('-d', '--device', type=str, default='0', help='cuda device(s)')
+    parser.add_argument('-it', '--iou-thres', type=float, default=0.65, help='YOLOv7 iou threshold')
+    parser.add_argument('-f', '--filename', type=str, default='out', help='filename for recordings')
+    parser.add_argument('-j', '--jetson', action='store_true', default=False, help='true if you are using the Nvidia Jetson Nano')
+    parser.add_argument('-l', '--label', action='store_true', default=False, help='true if you want to save labelled signs')
+    parser.add_argument('-n', '--name', type=str, default='camera', help='YOLOv7 result test directory name')
+    parser.add_argument('-p', '--project', type=str, default=os.path.join(parent, 'Models', 'YOLOv7', 'runs', 'test') , help='YOLOv7 project save directory')
+    parser.add_argument('-r', '--resolution', type=tuple, default=(1280, 720), help='camera resolution')
+    parser.add_argument('-rt', '--rotate', action='store_true', default=False, help='rotate frame for e-con camera')
+    parser.add_argument('-s', '--save-sign', action='store_true', default=False, help='save frames which contain signs')
+    parser.add_argument('-sh', '--save-hybrid', action='store_true', default=False, help='YOLOv7 save hybrid')
+    parser.add_argument('-st', '--save-txt', action='store_true', default=False, help='YOLOv7 save txt')
+    parser.add_argument('-w', '--weights', type=str, default=os.path.join(parent, 'Models', 'YOLOv7', 'last.pt') , help='YOLOv7 weights')
+    opt = parser.parse_args()
 
     opt.save_txt |= opt.save_hybrid
 
@@ -66,7 +86,7 @@ def main(opt):
     if not os.path.isdir(os.path.join(current, "ItalianSigns")):
         os.makedirs(os.path.join(current, "ItalianSigns"))
 
-    if not os.path.isdir(os.path.join(current, "ItalianSigns", 'images')):
+    if not os.path.isdir(os.path.join(current, "ItalianSigns" , 'images')):
         os.makedirs(os.path.join(current, "ItalianSigns", 'images'))
 
     if not os.path.isdir(os.path.join(current, "ItalianSigns", 'labels')):
@@ -75,18 +95,16 @@ def main(opt):
     if not os.path.isfile(os.path.join(current, "ItalianSigns", 'labels', 'ItalianSigns.csv')):
         f = open(os.path.join(current, "ItalianSigns", 'labels', 'ItalianSigns.csv'), 'w')
         writer = csv.writer(f)
-        writer.writerow(
-            ["filename", "x top left", "y top left", "x bottom right", "y bottom right", "speed limit", "valid"])
+        writer.writerow(["filename", "x top left", "y top left", "x bottom right",  "y bottom right", "speed limit", "valid"])
         f.close()
 
-    camera = RTCamera(opt.camera_device, fps=30, resolution=opt.resolution, cuda=True, auto_exposure=False,
-                      rotation=ROTATION)
+    camera = RTCamera(opt.camera_device, fps=30, resolution=opt.resolution, cuda=True, auto_exposure=False, rotation=ROTATION)
     camera.start()
 
     start_fps = time.monotonic()
     fps = 0
     listener.start()
-
+    
     sd = Sign_Detector()
     an = Annotator(*opt.resolution)
     an.org = (20, 50)
@@ -116,7 +134,7 @@ def main(opt):
 
     # Main infinite loop
     while True:
-        frame = camera.get_frame()
+        frame = camera.get_frame() 
 
         if camera.available():
             original = frame.copy()
@@ -125,7 +143,7 @@ def main(opt):
                 fps = camera.get_fps()
                 start_fps = time.monotonic()
 
-            if PRESSED_KEY == 'q':  # QUIT
+            if PRESSED_KEY == 'q': # QUIT
                 if label_file is not None:
                     label_file.close()
                 listener.stop()
@@ -133,56 +151,53 @@ def main(opt):
                 print("closing!")
                 break
 
-            elif PRESSED_KEY == 'r':  # REGISTER/STOP RECORDING
+            elif PRESSED_KEY == 'r': # REGISTER/STOP RECORDING
                 if not RECORDING:
                     print("recording started...")
-                    camera.register(os.path.join(current, "Recordings", "{}__{}.mp4".format(opt.filename,
-                                                                                            datetime.now().strftime(
-                                                                                                "%d_%m_%Y__%H_%M_%S"))))
+                    camera.register(os.path.join(current, "Recordings", "{}__{}.mp4".format(opt.filename, datetime.now().strftime("%d_%m_%Y__%H_%M_%S"))))
                     RECORDING = True
                 else:
                     camera.stop_recording()
                     print("recording stopped!")
                     RECORDING = False
 
-            elif PRESSED_KEY == 'g' and not RECORDING:  # CHANGE GAIN
+            elif PRESSED_KEY == 'g' and not RECORDING: # CHANGE GAIN
                 gain = int(input("please insert the gain: "))
                 camera.set_gain(gain)
 
-            elif PRESSED_KEY == 'e' and not RECORDING:  # CHANGE EXPOSURE
+            elif PRESSED_KEY == 'e' and not RECORDING: # CHANGE EXPOSURE
                 exp = int(input("please insert the exposure: "))
                 camera.set_exposure(exp)
 
-            elif PRESSED_KEY == 's' and not RECORDING:  # SAVE CURRENT FRAME
-                path = os.path.join(current, 'Calibration',
-                                    'frame_{}.jpg'.format(datetime.now().strftime("%d_%m_%Y__%H_%M_%S")))
+            elif PRESSED_KEY == 's' and not RECORDING: # SAVE CURRENT FRAME
+                path = os.path.join(current, 'Calibration', 'frame_{}.jpg'.format(datetime.now().strftime("%d_%m_%Y__%H_%M_%S")))
                 camera.save_frame(path)
 
                 print("saved frame {} ".format(path))
 
-            elif PRESSED_KEY == 'c' and not RECORDING:  # CALIBRATE CAMERA
+            elif PRESSED_KEY == 'c' and not RECORDING: # CALIBRATE CAMERA
                 print("Calibration in process, please wait...\n")
                 cv2.destroyAllWindows()
                 geometry = Geometry(os.path.join(current, 'Calibration'))
                 calibrated, mtx, dist, rvecs, tvecs = geometry.get_calibration()
                 camera.calibrate(calibrated, mtx, dist, rvecs, tvecs)
 
-            elif PRESSED_KEY == 'i':  # SHOW MEAN VALUE OF CURRENT FRAME
+            elif PRESSED_KEY == 'i': # SHOW MEAN VALUE OF CURRENT FRAME
                 print("Frame AVG value: {}".format(frame.mean(axis=(0, 1, 2))))
 
-            elif PRESSED_KEY == 'b':  # BLUR FRAME
+            elif PRESSED_KEY == 'b': # BLUR FRAME
                 BLUR = not BLUR
                 print("blur: {}".format(BLUR))
 
-            elif PRESSED_KEY == 't' and not RECORDING:  # APPLY TRANSFORMS TO FRAME
+            elif PRESSED_KEY == 't' and not RECORDING: # APPLY TRANSFORMS TO FRAME
                 TRANSFORMS = not TRANSFORMS
                 print("transform: {}".format(TRANSFORMS))
 
-            elif PRESSED_KEY == 'f' and not RECORDING:  # SHOW CHESSBOARD
+            elif PRESSED_KEY == 'f' and not RECORDING: # SHOW CHESSBOARD
                 CHESSBOARD = not CHESSBOARD
                 print("Chessboard: {}".format(CHESSBOARD))
                 cv2.destroyAllWindows()
-
+            
             if BLUR:
                 frame = preprocessor.GaussianBlur(frame, 1)
 
@@ -191,10 +206,9 @@ def main(opt):
 
             if CHESSBOARD:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                ret, corners = cv2.findChessboardCorners(gray, (7, 9),
-                                                         cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK)
+                ret, corners = cv2.findChessboardCorners(gray, (7,9), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK)
                 if ret:
-                    cv2.drawChessboardCorners(frame, (7, 9), corners, ret)
+                    cv2.drawChessboardCorners(frame, (7,9), corners, ret)
 
             if PRESSED_KEY != '':
                 PRESSED_KEY = ''
@@ -214,41 +228,36 @@ def main(opt):
 
                     for *xyxy, conf, cls in predn.tolist():
                         if conf > 0.7:
-                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1)  # xywh
-                            xywh = [int(x) for x in xywh]
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1) # xywh
+                            xywh = [ int(x) for x in xywh ]
                             x, y, w, h = xywh
 
                             detections.append((cls, xywh))
                             distance = Distance().get_Distance(xywh)
                             cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 0, 255), 2)
-                            cv2.circle(frame, (x + (w // 2), y + (h // 2)), 4, (40, 55, 255), 4)
-                            cv2.putText(frame, "{:.2f} {} {:.2f}".format(conf, names[int(cls)], distance),
-                                        (x + 5, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 1)
+                            cv2.circle(frame, (x + (w//2), y + (h//2)), 4, (40, 55, 255), 4)
+                            cv2.putText(frame, "{:.2f} {} {:.2f}".format(conf, names[int(cls)], distance), (x + 5, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 1)
 
                 # Tracking
-                hsvframe = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+                hsvframe =  cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
                 tracker.zero_objects()
                 for cls, box in detections:
                     x, y, w, h = box
-                    box[0] = int(box[0] + box[2] / 2)
-                    box[1] = int(box[1] + box[3] / 2)
+                    box[0] = int(box[0] + box[2]/2)
+                    box[1] = int(box[1] + box[3]/2)
                     id = tracker.update_obj(cls, box)
 
                     prediction, pts = tracker.track(hsvframe, box)
-                    cv2.putText(frame, "ID: {}".format(id), (x - 60, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6,
-                                (255, 0, 255), 1)
-                    cv2.putText(frame, "ID: {}".format(id),
-                                (int(prediction[0] - (0.5 * w)) + 5, int(prediction[1] - (0.5 * h)) + 20),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 1)
-                    cv2.rectangle(frame, (int(prediction[0] - (0.5 * w)), int(prediction[1] - (0.5 * h))),
-                                  (int(prediction[0] + (0.5 * w)), int(prediction[1] + (0.5 * h))), (0, 255, 0), 2)
+                    cv2.putText(frame, "ID: {}".format(id), (x - 60, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 1)
+                    cv2.putText(frame, "ID: {}".format(id), (int(prediction[0] - (0.5 * w)) + 5, int(prediction[1] - (0.5 * h)) + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 1)
+                    cv2.rectangle(frame, (int(prediction[0] - (0.5 * w)), int(prediction[1] - (0.5 * h))), (int(prediction[0] + (0.5 * w)), int(prediction[1] + (0.5 * h))), (0, 255, 0), 2)
                 tracker.clear_objects()
 
             # traffic sign detection
             height, width, _ = frame.shape
             h = height // 4
             w = width // 3
-            found, c, s, u = sd.detect(frame, h, w, show_results=False)
+            found, c, s, u = sd.detect(frame, h, w, show_results = False)
             if found and s != 0:
                 circles, speed, updates = c, s, u
 
@@ -258,19 +267,17 @@ def main(opt):
 
                     if opt.label:
                         fname = 'frame_{}.jpg'.format(datetime.now().strftime("%d_%m_%Y__%H_%M_%S"))
-                        fpath = os.path.join(current, "ItalianSigns", 'images', fname)
+                        fpath = os.path.join(current, "ItalianSigns" , 'images', fname)
                         if not os.path.isfile(fpath):
                             saved = cv2.imwrite(fpath, original)
                             if saved:
-                                sign_label = [fname, sign_bb[0][0], sign_bb[0][1], sign_bb[1][0], sign_bb[1][1], speed,
-                                              1]
+                                sign_label = [fname, sign_bb[0][0], sign_bb[0][1], sign_bb[1][0], sign_bb[1][1], speed, 1]
                                 label_writer.writerow(sign_label)
 
-                if opt.save_sign:
-                    path = os.path.join(current, 'signs',
-                                        'sign_{}.jpg'.format(datetime.now().strftime("%d_%m_%Y__%H_%M_%S")))
+                if opt.save_sign:    
+                    path = os.path.join(current, 'signs', 'sign_{}.jpg'.format(datetime.now().strftime("%d_%m_%Y__%H_%M_%S")))
                     cv2.imwrite(path, frame)
-
+                
             an.write(frame, speed, updates)
             cv2.putText(frame, str(fps) + " fps", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.imshow("frame", frame)
@@ -278,28 +285,3 @@ def main(opt):
     camera.stop()
     cv2.destroyAllWindows()
     print("closed")
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-b', '--batch-size', type=int, default=1, help='YOLOv7 batch-size')
-    parser.add_argument('-c', '--calibrate', action='store_true', default=False, help='true if you want to calibrate the camera')
-    parser.add_argument('-cd', '--camera-device', type=int, default=0, help='Camera device ID')
-    parser.add_argument('-ct', '--conf-thres', type=float, default=0.001, help='YOLOv7 conf threshold')
-    parser.add_argument('-d', '--device', type=str, default='0', help='cuda device(s)')
-    parser.add_argument('-it', '--iou-thres', type=float, default=0.65, help='YOLOv7 iou threshold')
-    parser.add_argument('-f', '--filename', type=str, default='out', help='filename for recordings')
-    parser.add_argument('-j', '--jetson', action='store_true', default=False, help='true if you are using the Nvidia Jetson Nano')
-    parser.add_argument('-l', '--label', action='store_true', default=False, help='true if you want to save labelled signs')
-    parser.add_argument('-n', '--name', type=str, default='camera', help='YOLOv7 result test directory name')
-    parser.add_argument('-p', '--project', type=str, default=os.path.join(parent, 'Models', 'YOLOv7', 'runs', 'test') , help='YOLOv7 project save directory')
-    parser.add_argument('-r', '--resolution', type=tuple, default=(1280, 720), help='camera resolution')
-    parser.add_argument('-rt', '--rotate', action='store_true', default=False, help='rotate frame for e-con camera')
-    parser.add_argument('-s', '--save-sign', action='store_true', default=False, help='save frames which contain signs')
-    parser.add_argument('-sh', '--save-hybrid', action='store_true', default=False, help='YOLOv7 save hybrid')
-    parser.add_argument('-st', '--save-txt', action='store_true', default=False, help='YOLOv7 save txt')
-    parser.add_argument('-w', '--weights', type=str, default=os.path.join(parent, 'Models', 'YOLOv7', 'last.pt') , help='YOLOv7 weights')
-    opt = parser.parse_args()
-
-    main(opt)
